421c654a8482893bc4783fb5817a8af47e7caedd
mlkmhmtbyk <biyikm16@itu.edu.tr>
2022-12-28 02:59:43 +0300

    First Migration, DbContext, Meal table and Model

---
+++ Data/Models/Meal.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace taofitAPI.Data.Models
+{
+    public class Meal
+    {
+        public int MealId { get; set; }
+        public string MealName { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Migrations/20221227235459_DBNamefixed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace taofitAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DBNamefixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
