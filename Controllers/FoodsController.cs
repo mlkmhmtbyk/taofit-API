6b636575f987ef6b49234b22285347879176ba1a
mlkmhmtbyk <biyikm16@itu.edu.tr>
2022-12-31 00:47:13 +0300

    Other CRUD operations handled for foods

--- Controllers/FoodsController.cs
+++ Controllers/FoodsController.cs
@@ -17,6 +17,12 @@ namespace taofitAPI.Controllers
             _foodsService = foodsService;
         }
 
+        [HttpGet("GetFoods")]
+        public IActionResult GetFoods(){
+            var allFoods = _foodsService.GetFoods();
+            return Ok(allFoods);
+        }
+
         [HttpPost("AddFood")]
         public IActionResult AddFood([FromBody]Food food)
         {
@@ -24,5 +30,25 @@ namespace taofitAPI.Controllers
             return Ok();
         }
 
+        [HttpGet("GetFood{foodId}")]
+        public IActionResult GetFood(int foodId)
+        {
+            var food = _foodsService.GetFood(foodId);
+            return Ok(food);
+        }
+
+        [HttpPut("UpdateFood{foodId}")]
+        public IActionResult UpdateFood(int foodId, [FromBody] Food food)
+        {
+            var updatedFood = _foodsService.UpdateFood(foodId, food);   
+            return Ok(updatedFood);
+        }
+
+         [HttpDelete("DeleteFood{foodId}")]
+        public IActionResult DeleteFood(int foodId)
+        {
+            _foodsService.DeleteFood(foodId);
+            return Ok();
+        }
     }
 }
--- Services/FoodsService.cs
+++ Services/FoodsService.cs
@@ -32,5 +32,35 @@ namespace taofitAPI.Services
             _context.Foods.Add(_food);
             _context.SaveChanges();
         }
+
+        public Food GetFood(int foodId)
+        {   
+            return _context.Foods.Find(foodId);
+        }
+
+        public Food UpdateFood(int foodId, Food food)
+        {
+            var _food = _context.Foods.FirstOrDefault(f => f.FoodId == foodId);
+            if(_food != null)
+            {
+                _food.FoodName = food.FoodName;
+                _food.Amount = food.Amount;
+                _food.Calory = food.Calory;
+
+                _context.SaveChanges();
+            }
+
+            return _food;
+        }
+
+        public void DeleteFood(int foodId)
+        {
+            var _food = _context.Foods.FirstOrDefault(f => f.FoodId == foodId);
+            if(_food != null)
+            {
+                _context.Foods.Remove(_food);
+                _context.SaveChanges();
+            }
+        }
     }
 }
